9edd9d22461a11c6accade6fa02089b509df90d8
Bogdan Popa <bogdan@defn.io>
2019-06-04 15:14:51 +0300

    github: use dank mono

--- github.js
+++ github.js
@@ -1,8 +1,8 @@
 const style = document.createElement("style");
 style.type = "text/css";
 style.innerHTML = `
-code, pre, .blob-code {
-  font-family: "sf mono" !important;
+code, pre, .blob-code, .blob-code-inner {
+  font-family: "dank mono", "sf mono" !important;
 }
 `;
 
